5d303d2541b2e88b769d995c537d2bec31034bba
Andrew Voronov <avoronov@madisonpg.com>
2019-10-09 16:29:02 +0300

    initial commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,51 @@
-import '../styles/index.scss';
+import '../styles/index.css';
+import 'vanderlee-colorpicker/jquery.colorpicker.css';
+ 
+cssVars({
+ 
+  variables: {
+      '--main-background-color': 'black',
+      '--secondary-background-color': 'green'
+  },
+  // updateDOM: true
 
-console.log('webpack starterkit');
+});
+
+$(function() {  
+  $('#main-bg-color-input').colorpicker({
+    ok: function(event, color) {
+      cssVars({
+        variables: {
+            '--main-background-color': "#" + color.formatted
+        }
+    });
+    }
+  });
+});
+
+$(function() {  
+  $('#secondary-bg-color-input').colorpicker({
+    ok: function(event, color) {
+      cssVars({
+        variables: {
+            '--secondary-background-color': "#" + color.formatted
+        }
+    });
+    }
+  });
+});
+
+const customFont = document.querySelector("#custom-font");
+
+customFont.addEventListener('change',  function(e) {
+    cssVars({
+        variables: {
+            '--first-font': `${this.value}`
+        }
+    });
+  });
+
+
+ 
+ 
+  
